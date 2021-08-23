51cb50d0e902a23e0293c4042b885ab595a83ede
Isabella <wileyisabella@gmail.com>
2021-08-23 14:08:34 -0400

    contact section layout done

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -2,7 +2,17 @@
 function Contact(){
     return(
         <div className="contact">
-            <h1>Contact</h1>
+            <h1 className='title'>Let's Connect!</h1>
+            <div className='contactCard'>
+                <div className='contactColumns'>
+                    <h5>wileyisabella@gmail.com</h5>
+                    <a href='https://www.linkedin.com/in/isabella-wiley-b0b719164/'><h5>LinkedIn</h5></a>
+                </div>
+                <div className='contactColumns'>
+                    <a href='https://github.com/isabellawiley'><h5>GitHub</h5></a>
+                    <a href='https://wileyisabella.medium.com/'><h5>Blogs</h5></a>
+                </div>
+            </div>
         </div>
     )
 }
