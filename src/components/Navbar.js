30f4c5df02d89b4cdda5ee67dda0e04386a6d85f
Isabella <wileyisabella@gmail.com>
2023-04-17 19:03:45 -0400

    navbar created and styled

---
+++ src/components/Navbar.js
@@ -0,0 +1,28 @@
+import { useState } from "react";
+import '../styling/navbar.css';
+
+function Navbar({}){
+    const [isActive, setIsActive] = useState(false);
+
+    return(
+        <nav className="navigation">
+            <div className="logo"><a href='#'>isabella</a></div>
+            <div className={isActive ? 'change' : 'menu'} onClick={() => setIsActive(!isActive)}>
+                <div className="bar1"></div>
+                <div className="bar2"></div>
+                <div className="bar3"></div>
+            </div>
+            <div className={isActive ? 'navbar expanded' : 'navbar'}>
+                <ul>
+                    <li><a href="#About">About</a></li>
+                    <li><a href="#Experience">Experience</a></li>
+                    <li><a href="#Projects">Projects</a></li>
+                    <li><a href="#Contact">Contact</a></li>
+                    <li><a href="#Cats">Cats</a></li>
+                </ul>
+            </div>
+        </nav>
+    )
+}
+
+export default Navbar;
