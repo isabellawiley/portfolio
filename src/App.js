c51d4f8860f64fba2fc9bf4d8f849fb9f9199ff5
Isabella <wileyisabella@gmail.com>
2023-04-18 20:10:38 -0400

    experience section created and minimally styled

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import About from "./components/About";
+import Experience from "./components/Experience";
 import Navbar from "./components/Navbar";
 
 function App() {
@@ -6,6 +7,7 @@ function App() {
     <div>
       <Navbar />
       <About />
+      <Experience />
       <h1>HELLO WORLD!</h1>
     </div>
   );
