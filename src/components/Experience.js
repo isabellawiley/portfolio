5f4b9aaa659a7c3d664607fed84d526fd4f8f474
Isabella <wileyisabella@gmail.com>
2023-05-04 14:44:51 -0400

    changed app title; edited viewbox for first experience svg

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -10,7 +10,7 @@ function Experience(){
 
     return(
         <div id='Experience' className="experience section">
-            <svg preserveAspectRatio="none" viewBox="0 0 100 102" height="75" width="100%" version="1.1" xmlns="http://www.w3.org/2000/svg" class="svgcolor-light">
+            <svg preserveAspectRatio="none" viewBox="0 1 100 102" height="75" width="100%" version="1.1" xmlns="http://www.w3.org/2000/svg" class="svgcolor-light">
                 <path d="M0 0 L50 100 L100 0 Z" fill="#FFFCF4" stroke="#FFFCF4"></path>
             </svg>
             <h1>Experience</h1>
